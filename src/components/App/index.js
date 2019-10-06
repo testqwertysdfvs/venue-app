45e8fff951f53c87791cda6d2ecfd9a58e437463
Dmitry Buravtsov <dmitry@MacBook-Pro-Dmitriy.local>
2019-10-06 22:41:49 +0300

    template created

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from "./style";
+
+function App() {
+    return (
+        <div>
+            <Text>React app</Text>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/App/style.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const Text = styled.h1`
+font-weight: 900;
+`;
